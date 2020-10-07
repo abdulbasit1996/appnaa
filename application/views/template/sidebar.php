e53378e3b8c62b05639065dbbcceb4dd37d2e5ea
unknown <m.5y4msulpaiton@gmail.com>
2020-10-08 05:12:22 +0700

    membuat menu & tambah logo

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -2,10 +2,11 @@
 
   <!-- Sidebar - Brand -->
   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
+    <div class="sidebar-brand-icon">
+      <!-- <i class="fas fa-laugh-wink"></i> -->
+      <img src="assets/img/LOGONAA.png" alt="" width="40px">
     </div>
-    <div class="sidebar-brand-text mx-3">Mahrom</div>
+    <div class="sidebar-brand-text mx-3">NAA App</div>
   </a>
 
   <!-- Divider -->
@@ -15,7 +16,7 @@
   <li class="nav-item active">
     <a class="nav-link" href="beranda">
       <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Beranda</span></a>
+      <span>BERANDA</span></a>
   </li>
 
   <!-- Divider -->
@@ -23,81 +24,102 @@
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Menu
+    LIST MENU
   </div>
 
   <!-- Nav Item - Pages Collapse Menu -->
+  <!-- Menu bagian KEPEGURUSAN -->
   <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-      <i class="fas fa-fw fa-cog"></i>
-      <span>Components</span>
+      <i class="fas fa-users fa-cog"></i>
+      <span>PENGURUS</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Components:</h6>
-        <a class="collapse-item" href="buttons.html">Buttons</a>
-        <a class="collapse-item" href="cards.html">Cards</a>
+        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+        <a class="collapse-item" href="buttons.html">Menu 1</a>
+        <a class="collapse-item" href="cards.html">Menu 2</a>
       </div>
     </div>
   </li>
 
   <!-- Nav Item - Utilities Collapse Menu -->
+  <!-- Menu bagian KESEHATAN -->
   <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-      <i class="fas fa-fw fa-wrench"></i>
-      <span>Utilities</span>
+      <i class="fas fa-fw fa-user-md"></i>
+      <span>KESEHATAN</span>
     </a>
     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Utilities:</h6>
-        <a class="collapse-item" href="utilities-color.html">Colors</a>
-        <a class="collapse-item" href="utilities-border.html">Borders</a>
-        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-        <a class="collapse-item" href="utilities-other.html">Other</a>
+        <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+        <a class="collapse-item" href="">Kelola Kesehatan Santri</a>
+        <a class="collapse-item" href="">Kelola Level Perawatan</a>
+        <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a> -->
+        <!-- <a class="collapse-item" href="utilities-other.html">Other</a> -->
       </div>
     </div>
   </li>
+
+
 
   <!-- Divider -->
   <hr class="sidebar-divider">
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Addons
+    LIST MENU KEAMANAN
   </div>
 
-  <!-- Nav Item - Pages Collapse Menu -->
+  <!-- Menu bagian PELANGGARAN -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities1" aria-expanded="true" aria-controls="collapseUtilities1">
+      <i class="fas fa-fw fa-shield-alt"></i>
+      <span>PELANGGARAN</span>
     </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+    <div id="collapseUtilities1" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="login.html">Login</a>
-        <a class="collapse-item" href="register.html">Register</a>
-        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-        <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="404.html">404 Page</a>
-        <a class="collapse-item" href="blank.html">Blank Page</a>
+        <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+        <a class="collapse-item" href="">Menu 1</a>
+        <a class="collapse-item" href="">Menu 2</a>
+        <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a> -->
+        <!-- <a class="collapse-item" href="utilities-other.html">Other</a> -->
       </div>
     </div>
   </li>
 
-  <!-- Nav Item - Charts -->
+  <!-- Menu bagian PERIZINAN -->
   <li class="nav-item">
-    <a class="nav-link" href="charts.html">
-      <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities2" aria-expanded="true" aria-controls="collapseUtilities2">
+      <i class="fas fa-fw fa-clipboard-list"></i>
+      <span>PERIZINAN</span>
+    </a>
+    <div id="collapseUtilities2" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+        <a class="collapse-item" href="">Menu 1</a>
+        <a class="collapse-item" href="">Menu 2</a>
+        <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a> -->
+        <!-- <a class="collapse-item" href="utilities-other.html">Other</a> -->
+      </div>
+    </div>
   </li>
 
-  <!-- Nav Item - Tables -->
+  <!-- Menu bagian MAHROM -->
   <li class="nav-item">
-    <a class="nav-link" href="tables.html">
-      <i class="fas fa-fw fa-table"></i>
-      <span>Tables</span></a>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities3" aria-expanded="true" aria-controls="collapseUtilities3">
+      <i class="fas fa-fw fa-users"></i>
+      <span>MAHROM</span>
+    </a>
+    <div id="collapseUtilities3" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+        <a class="collapse-item" href="">Menu 1</a>
+        <a class="collapse-item" href="">Menu 2</a>
+        <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a> -->
+        <!-- <a class="collapse-item" href="utilities-other.html">Other</a> -->
+      </div>
+    </div>
   </li>
 
   <!-- Divider -->
